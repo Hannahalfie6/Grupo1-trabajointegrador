e8dd9654b5364a1d2aaeef387df5bb3f8cb5cbfb
magui-sanchez <sanchezma@udesa.edu.ar>
2025-06-04 12:32:02 -0300

    detalle series

--- js/detalleSerie.js
+++ js/detalleSerie.js
@@ -1,20 +1,19 @@
-//traemos serie
 let querySelector = location.search;
-let queryParams = new URLSearchParams(querySring);
-let idSerie = queryParams.get('id');
+let querySelectorTo = new URLSearchParams(querySelector);
+let idSerie = querySelectorTo.get('id');
 
-fetch('https://api.themoviedb.org/3/tv/series_id?language=en-US')
+fetch(`https://api.themoviedb.org/3/tv/${idSerie}?api_key=90b45a60c2f1bb623a150a6f0011fbcb&language=es-US`)
 .then(function(response){
     return response.json();
 })
 .then(function(data){
     console.log(data);
-    let main = document.querySelector('.mainindex');
-    let detalles = '';
-    for(let i=0; i<  data.length; i++){ //ALGO MAS EN EL LENGTH//
-        detalles += //FALTA EL LINK ACA
-    };
-    main.innerHTML= `
+    let main =document.querySelector('.mainindex');
+    let generos = '';
+    for (let i=0; i < data.genres.length; i++){
+        generos += `<a href="./detalleGenero.html?id=${data.genres[i].id}&name=${data.genres[i].name}&type=serie">${data.genres[i].name} </a>`;
+    }
+    main.innerHTML = `
     <img class="detalleSerie" src="./img/${}.jpg" alt="" srcset="">
         <h1 class="detalle-titulo"> ${} </h1>
         <h2 class="detalle-calificacion">Calificacion: ${}</h2>
@@ -22,5 +21,5 @@ fetch('https://api.themoviedb.org/3/tv/series_id?language=en-US')
         <p class="detalle-descripcion"> ${}</p>
         <h3 class="detalle-genero">Genero: ${generos}>Acción</a></h3>
     `;
-    })
-.catch(error)
+})
+.catch()
