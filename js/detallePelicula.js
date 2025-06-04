0f2a9e02bb9ede3bdfd2f91a2b4f5ba6bfef7df0
delfina-iribarren <diribarren@udesa.edu.ar>
2025-06-03 22:53:35 -0300

    termine detallePelicula

--- js/detallePelicula.js
+++ js/detallePelicula.js
@@ -3,7 +3,7 @@ let querySelector = location.search;
 let querySelectorTo = new URLSearchParams(querySelector);
 let idPelicula = querySelectorTo.get('id');
 
-fetch("https://api.themoviedb.org/3/movie/${idPelicula}?api_key=90b45a60c2f1bb623a150a6f0011fbcb&language=es-US")
+fetch(`https://api.themoviedb.org/3/movie/${idPelicula}?api_key=90b45a60c2f1bb623a150a6f0011fbcb&language=es-US"`)
 .then(function(response){
     return response.json();
 })
@@ -12,11 +12,11 @@ fetch("https://api.themoviedb.org/3/movie/${idPelicula}?api_key=90b45a60c2f1bb62
     let main =document.querySelector('.mainindex');
     let generos = '';
     for (let i=0; i < data.genres.length; i++){
-        generos += '<a href="./generosPeliculas.html?id=${data.genres[i].id}&name=${data.genres[i].name}">${data.genres[i].name}</a>';
+        generos += '<a href="./detalleGenero.html?id=${data.genres[i].id}&name=${data.genres[i].name}&type=pelicula">${data.genres[i].name} </a>';
     }
     main.innerHTML = `
     <img class="detallePelicula" src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="">
-    <h1 class="detalle-titulo">${data.title} 1985</h1>
+    <h1 class="detalle-titulo">${data.title} </h1>
     <h2 class="detalle-calificacion">Calificacion: ${data.vote_average}/10</h2>
     <h3 class="detalle-info">Fecha de estreno: ${data.release_date}</h3>
     <h3 class="detalle-info">Duracion: ${data.runtime} minutos </h3>
